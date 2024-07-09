0aeafc87962900e92eead2fe2058e8da2fcef9a7
Jinnyzinny <ddocdoli@jbnu.ac.kr>
2024-07-09 21:10:20 +0900

    2023 정보검색 프로젝트
    PotterStemmer
    IR Termproject
    Python Crawler
    3가지 프로젝트

---
+++ hw2_PorterStemmer_이진형/snowball Stemmer.py
@@ -0,0 +1,9 @@
+from nltk.stem import SnowballStemmer
+
+stemmer = SnowballStemmer("english")
+
+print(stemmer.stem("working"), stemmer.stem("works"), stemmer.stem("worked"))
+print(stemmer.stem("amusing"), stemmer.stem("amuses"), stemmer.stem("amused"))
+print(stemmer.stem("happier"), stemmer.stem("happiest"))
+print(stemmer.stem("fancier"), stemmer.stem("fanciest"))
+print(stemmer.stem("was"), stemmer.stem("love"))
